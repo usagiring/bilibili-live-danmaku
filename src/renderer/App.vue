283ceda5be37761429d08fca9b7a1dd935194f42
hling51325 <hling51325@163.com>
2021-06-03 22:31:29 +0800

    fix

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <link
-      href="../node_modules/animate.css/animate.compat.css"
-      rel="stylesheet"
-      type="text/css"
-    />
     <router-view></router-view>
   </div>
 </template>
